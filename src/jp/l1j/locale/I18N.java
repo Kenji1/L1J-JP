99b4ed9ca96ad42c057e0d05618116094131de90
l1j.hack <l1j.hack@698199e4-35c4-443b-82c4-7f4b4ae73c13>
2013-03-17 21:57:28 +0000

    国際化i18n（中間コミット） refs #306 ・国際化クラスのリファクタリング ・香港（zh_HK、中国語繁体字）を追加（台湾をコピー）
    git-svn-id: http://svn.l1j.org/l1j/trunk@287 698199e4-35c4-443b-82c4-7f4b4ae73c13

--- src/jp/l1j/locale/I18N.java
+++ src/jp/l1j/locale/I18N.java
@@ -1,211 +1,53 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package jp.l1j.locale;
 
 import java.util.ResourceBundle;
 
-/**
- *
- * @author Administrator
- */
 public class I18N {
 	
 	private static XMLResourceBundleControl control = new XMLResourceBundleControl();
-	
 	private static ResourceBundle bundle = ResourceBundle.getBundle("./locale/lang", control);
 
-	/**
-	 * <li>ja_JP 利用メモリ: %s MB</li>
-	 * <li>en_US Memory usage: %s MB</li>
-	 * <li>zh_TW 記憶體使用量: %s MB</li>
-	 * <li>zh_CN 内存使用量: %s MB</li>
+	/*
+	 * jp.l1j.server.GameServer.java
 	 */
 	public static final String I18N_MEMORY_USEAGE = bundle.getString("MemoryUsage");
-	
-	/**
-	 * <li>ja_JP クライアント接続待機中...</li>
-	 * <li>en_US Waiting for client connection...</li>
-	 * <li>zh_TW 正在等待用戶端連線...</li>
-	 * <li>zh_CN 正在等待用户连接...</li>
-	 */
 	public static final String I18N_WAITING_FOR_CLIENT = bundle.getString("WaittingForClient");
-	
-	/**
-	 * <li>ja_JP %s 接続試行中</li>
-	 * <li>en_US %s Try to connect</li>
-	 * <li>zh_TW %s 正在嘗試連線</li>
-	 * <li>zh_CN %s 正在尝试连线</li>
-	 */
 	public static final String I18N_TRYING_TO_CONNECTION = bundle.getString("TryingToConnection");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US Banned IP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_BANNED_IP = bundle.getString("BannedIP");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_GENERATE_SERVER_SOCKET = bundle.getString("GenerateServerSocket");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_SERVER_SETTINGS = bundle.getString("ServerSettings");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_EXP = bundle.getString("Exp");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_LAWFUL = bundle.getString("Lawful");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_KARMA = bundle.getString("Karma");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_ITEM_DROP = bundle.getString("ItemDrop");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_ADENA_DROP = bundle.getString("AdenaDrop");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_ENCHANT_WEAPON = bundle.getString("EnchantWeapon");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_ENCHANT_ARMOR = bundle.getString("EnchantArmor");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_ENCHANT_ATTRIBUTE = bundle.getString("EnchantAttribute");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_GLOBAL_CHAT = bundle.getString("GlobalChat");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_PVP = bundle.getString("PvP");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_NON_PVP = bundle.getString("NonPvP");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_MAX_USERS = bundle.getString("MaxUsers");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_LOADING_COMPLETE = bundle.getString("LoadingComplete");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_SHUTDOWN_THE_SERVER = bundle.getString("ShutdownTheServer");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_PLEASE_LOGOUT = bundle.getString("PleaseLogout");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_SHUTDOWN_AFTER_FEW_MINUTES = bundle.getString("ShutdownAfterFewMinutes");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_SHUTDOWN_AFTER_FEW_SECONDS = bundle.getString("ShutdownAfterFewSeconds");
-	
-	/**
-	 * <li>ja_JP 禁止IP(%s)</li>
-	 * <li>en_US BannedIP(%s)</li>
-	 * <li>zh_TW 禁止IP(%s)</li>
-	 * <li>zh_CN 禁止IP(%s)</li>
-	 */
 	public static final String I18N_SHUTDOWN_ABORT = bundle.getString("ShutdownAbort");
 		
 }
--- src/jp/l1j/locale/XMLResourceBundleControl.java
+++ src/jp/l1j/locale/XMLResourceBundleControl.java
@@ -1,23 +1,32 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package jp.l1j.locale;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class XMLResourceBundleControl extends ResourceBundle.Control {
-
+	
 	public List<String> getFormats(String baseName) {
 		if (baseName == null) {
 			throw new NullPointerException();
@@ -25,14 +34,9 @@ public class XMLResourceBundleControl extends ResourceBundle.Control {
 		return Arrays.asList("xml");
 	}
 	
-	public ResourceBundle newBundle(String baseName,
-			Locale locale,
-			String format,
-			ClassLoader loader,
-			boolean reload)
-			throws IllegalAccessException,
-			InstantiationException,
-			IOException {
+	public ResourceBundle newBundle(String baseName, Locale locale,
+			String format, ClassLoader loader, boolean reload)
+			throws IllegalAccessException, InstantiationException, IOException {
 		if (baseName == null || locale == null || format == null || loader == null) {
 			throw new NullPointerException();
 		}
@@ -40,6 +44,7 @@ public class XMLResourceBundleControl extends ResourceBundle.Control {
 		if (format.equals("xml")) {
 			String bundleName = toBundleName(baseName, locale);
 			String resourceName = toResourceName(bundleName, format);
+			
 			/*
 			InputStream stream = null;
 			if (reload) {
@@ -57,11 +62,12 @@ public class XMLResourceBundleControl extends ResourceBundle.Control {
 				stream = loader.getResourceAsStream(resourceName);
 			}
 			*/
-			
+					
 			// TODO start
 			File file = new File(resourceName.replace("//", "./"));
 			InputStream stream = new FileInputStream(file);
 			// TODO end
+			
 			if (stream != null) {
 				BufferedInputStream bis = new BufferedInputStream(stream);
 				bundle = new XMLResourceBundle(bis);
