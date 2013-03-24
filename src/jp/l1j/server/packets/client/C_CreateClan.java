4dcd616fa545f8ca54f031e2fdd0f59b2b6a5b3e
l1j.hack <l1j.hack@698199e4-35c4-443b-82c4-7f4b4ae73c13>
2013-03-24 04:12:36 +0000

    30,000アデナを所持していなくてもクランを創設できてしまう。fixes #315
    git-svn-id: http://svn.l1j.org/l1j/trunk@306 698199e4-35c4-443b-82c4-7f4b4ae73c13

--- src/jp/l1j/server/packets/client/C_CreateClan.java
+++ src/jp/l1j/server/packets/client/C_CreateClan.java
@@ -18,46 +18,48 @@ package jp.l1j.server.packets.client;
 import java.util.logging.Logger;
 import jp.l1j.server.ClientThread;
 import jp.l1j.server.datatables.ClanTable;
-import jp.l1j.server.model.instance.L1PcInstance;
 import jp.l1j.server.model.L1Clan;
 import jp.l1j.server.model.L1World;
+import jp.l1j.server.model.instance.L1PcInstance;
+import static jp.l1j.server.model.item.L1ItemId.ADENA;
 import jp.l1j.server.packets.server.S_ServerMessage;
 
-// Referenced classes of package jp.l1j.server.clientpackets:
-// ClientBasePacket
-
 public class C_CreateClan extends ClientBasePacket {
-
 	private static final String C_CREATE_CLAN = "[C] C_CreateClan";
+	
 	private static Logger _log = Logger.getLogger(C_CreateClan.class.getName());
 
-	public C_CreateClan(byte abyte0[], ClientThread clientthread)
-			throws Exception {
+	public C_CreateClan(byte abyte0[], ClientThread clientthread) throws Exception {
 		super(abyte0);
 		String s = readS();
 		int i = s.length();
-
-		L1PcInstance l1pcinstance = clientthread.getActiveChar();
-		if (l1pcinstance.isCrown()) { // プリンスまたはプリンセス
-			if (l1pcinstance.getClanid() == 0) {
-
-				for (L1Clan clan : L1World.getInstance().getAllClans()) { // 同じクラン名をチェック
-					if (clan.getClanName().toLowerCase()
-							.equals(s.toLowerCase())) {
-						l1pcinstance.sendPackets(new S_ServerMessage(99)); // \f1同じ名前の血盟が存在します。
-						return;
-					}
-				}
-				L1Clan clan = ClanTable.getInstance().createClan(l1pcinstance,
-						s); // クラン創設
-				if (clan != null) {
-					l1pcinstance.sendPackets(new S_ServerMessage(84, s)); // \f1%0血盟が創設されました。
-				}
-			} else {
-				l1pcinstance.sendPackets(new S_ServerMessage(86)); // \f1すでに血盟を結成されているので作成できません。
+		L1PcInstance pc = clientthread.getActiveChar();
+		if (!pc.isCrown()) {
+			pc.sendPackets(new S_ServerMessage(85));
+			// \f1プリンスとプリンセスだけが血盟を創設できます。
+			return;
+		}
+		if (pc.getClanid() != 0) {
+			pc.sendPackets(new S_ServerMessage(86));
+			// \f1すでに血盟を結成されているので作成できません。
+			return;
+		}
+		if (!pc.getInventory().checkItem(ADENA, 30000)) {
+			pc.sendPackets(new S_ServerMessage(189));
+			// 189 \f1アデナが不足しています。
+			return;
+		}
+		for (L1Clan clan : L1World.getInstance().getAllClans()) {
+			if (clan.getClanName().toLowerCase().equals(s.toLowerCase())) {
+				pc.sendPackets(new S_ServerMessage(99));
+				// \f1同じ名前の血盟が存在します。
+				return;
 			}
-		} else {
-			l1pcinstance.sendPackets(new S_ServerMessage(85)); // \f1プリンスとプリンセスだけが血盟を創設できます。
+		}
+		L1Clan clan = ClanTable.getInstance().createClan(pc, s); // クラン創設
+		if (clan != null) {
+			pc.sendPackets(new S_ServerMessage(84, s));
+			// \f1%0血盟が創設されました。
 		}
 	}
 
@@ -65,5 +67,4 @@ public class C_CreateClan extends ClientBasePacket {
 	public String getType() {
 		return C_CREATE_CLAN;
 	}
-
 }
