0f6aef1af5f36c64d79d2d37456600d524754287
l1j.hack <l1j.hack@698199e4-35c4-443b-82c4-7f4b4ae73c13>
2013-03-31 11:41:14 +0000

    ban_ips テーブルのスキーマを修正 refs #333
    git-svn-id: http://svn.l1j.org/l1j/trunk@315 698199e4-35c4-443b-82c4-7f4b4ae73c13

--- db/schema/mysql/ban_ips.sql
+++ db/schema/mysql/ban_ips.sql
@@ -5,6 +5,6 @@ SET FOREIGN_KEY_CHECKS=0;
 CREATE TABLE IF NOT EXISTS `ban_ips` (
   `ip` varchar(255) NOT NULL DEFAULT '',
   `host` varchar(255) NOT NULL DEFAULT '',
-  `mask` int(10) DEFAULT '32',
+  `mask` int(10) unsigned DEFAULT '32',
   PRIMARY KEY (`ip`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
