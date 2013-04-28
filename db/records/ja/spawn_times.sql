224f6bde1dcc6a44f11429c3b56f542afa20b8cf
l1j.hack <l1j.hack@698199e4-35c4-443b-82c4-7f4b4ae73c13>
2013-04-28 18:48:34 +0000

    旧DB構築方法に関連するファイルを削除
    git-svn-id: http://svn.l1j.org/l1j/trunk@365 698199e4-35c4-443b-82c4-7f4b4ae73c13

--- db/records/ja/spawn_times.sql
+++
@@ -1,8 +0,0 @@
--- ----------------------------
--- Records of spawn_times
--- ----------------------------
-INSERT INTO `spawn_times` VALUES (62086,'恨みに満ちたソルジャー ゴースト','18:00:00','04:00:00',1);
-INSERT INTO `spawn_times` VALUES (62087,'恨みに満ちたソルジャー ゴースト','18:00:00','04:00:00',1);
-INSERT INTO `spawn_times` VALUES (62088,'恨みに満ちたゴースト','18:00:00','04:00:00',1);
-INSERT INTO `spawn_times` VALUES (62089,'恨みに満ちたゴースト','18:00:00','04:00:00',1);
-INSERT INTO `spawn_times` VALUES (62092,'恨みに満ちたハメル将軍','18:00:00','04:00:00',1);
--- db/records/ko/spawn_times.sql
+++
@@ -1,8 +0,0 @@
--- ----------------------------
--- Records of spawn_times
--- ----------------------------
-INSERT INTO `spawn_times` VALUES (62086, '원한에 찬 병사 유령', '18:00:00', '04:00:00', 1);
-INSERT INTO `spawn_times` VALUES (62087, '원한에 찬 병사 유령', '18:00:00', '04:00:00', 1);
-INSERT INTO `spawn_times` VALUES (62088, '원한에 찬 남자 유령', '18:00:00', '04:00:00', 1);
-INSERT INTO `spawn_times` VALUES (62089, '원한에 찬 남자 유령', '18:00:00', '04:00:00', 1);
-INSERT INTO `spawn_times` VALUES (62092, '원한에 찬 하멜 장군', '18:00:00', '04:00:00', 1);
